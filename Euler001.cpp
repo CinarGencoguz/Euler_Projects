58a370e4e12ed77204b8db5dbfa7b71452c7ba14
Çınar Gençoğuz <cinar.gencoguz@mavinci.local>
2026-09-02 16:14:53 +0300

    refactor:Euler001

--- Euler001.cpp
+++ Euler001.cpp
@@ -1,16 +1,15 @@
+// OBJECTIVE
+// Find the sum of all natural numbers below 1000 that are multiples of 3 or 5.
+
 #include <iostream>
 using namespace std;
 int sum=0;
 
-main()
+int main()
 {
     for(int i=3; i<1000; i++)
     {
-        if(i%3==0&&i%5==0)
-          sum+=i;
-        else if(i%3==0)
-          sum+=i;
-        else if(i%5==0)
+        if(i%3==0 || i%5==0)
           sum+=i;
     }
     cout <<"sum = "<< sum << endl;
