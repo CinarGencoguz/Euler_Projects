28f73c9a056d5d8df1c7dfe79362f8a1c509038c
Çınar Gençoğuz <cinar.gencoguz@mavinci.local>
2026-09-03 12:06:41 +0300

    refactor:Euler039

--- src/Euler039.cpp
+++ src/Euler039.cpp
@@ -5,29 +5,31 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
-int maxleng, leng;
+int maxNOS, nOS;
 double a,result;
-
+// nOS is the number of solutions ,maxNOS is the max number of solutions
+// a is hypotenuse , b is shorter side
 int main()
 {
-    for (int i = 2; i < 1000; i++)
+    for (int p = 2; p < 1000; p++)
     {
-        leng=0;
-        for (int k = 2; k < 1000; k++)
+        nOS=0;
+        for (int b = 2; b < 500; b++) // threshold is 500 because I consider that  b is the shorter side 
         {
-            for (int t = k+1; t < 1000-k; t++)
+            for (int c = b+1; c < 1000-b; c++)// Decreasing the upper bound to reduce execution time
             {
-                a=sqrt(k*k+t*t);
-                if (a==(int)a&&k+t+a==i)
+                a=sqrt(b*b+c*c);
+                if (a==(int)a&&b+c+a==p) // Program checks whether the hypotenuse is integer or decimal number
                 {
-                    leng++;
-                    if (leng>maxleng)
+                    nOS++;
+                    if (nOS>maxNOS)
                     {
-                        result=i;maxleng=leng;
+                        result=p;
+                        maxNOS=nOS;
                     }
                 }
             } 
         }   
     } 
-    cout << result;
+    cout << result ;
 }
