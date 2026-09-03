db1002e9053e2e228054e0d48670cc5c05dd5bb3
Çınar Gençoğuz <cinar.gencoguz@mavinci.local>
2026-09-03 11:38:36 +0300

    refactor:Euler019

--- src/Euler019.cpp
+++ src/Euler019.cpp
@@ -5,19 +5,19 @@
 
 #include <iostream>
 using namespace std;
-int day = 2;
+int day = 2; //1 Jan 1901 is Tuesday
 int result = 0;
 int month[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
-
-int main() {
-    day = 2; result = 0;
+//day numberization start with as 0=sunday
+int main() 
+{
     for (int year = 1901; year <= 2000; year++) 
 	{
 		month[1]=28;
-        if (year%4==0)
-			month[1]=29;
-        for (int m = 0; m < 12; m++) {
-            if (day == 0) 
+        if (year%4==0)  month[1]=29;     //looking for February's number of days
+        for (int m = 0; m < 12; m++) 
+        {
+            if (day == 0)               // looking for every month first day
 			{ 
                 result++;
                 cout << year << "  " << m+1 << endl;
