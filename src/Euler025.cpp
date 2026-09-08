5517d8a329976f7dfa35c8e38518ed88af278e43
Çınar Gençoğuz <cinargencoguz01@outlook.com>
2026-09-08 14:03:37 +0300

    refactor:Euler025

--- src/Euler025.cpp
+++ src/Euler025.cpp
@@ -1,12 +1,12 @@
 // OBJECTIVE
-// The Fibonacci sequence is defined by F1 = 1, F2 = 1, Fn = Fn-1 + Fn-2.
+// The Fibonacci sequence is defined by F1 = 1, F2 = 1, Fn3 = Fn1 + Fn2.
 // The 12th term, F12 = 144, is the first term to contain three digits.
 // What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
 
 #include <iostream>
 using namespace std;
 int num1[1002]={0},num2[1002]={0};
-int digit,trash,result;
+int digit,temp,result;
 
 int main() 
 {
@@ -14,13 +14,13 @@ int main()
     while (digit<1000)
     {
         result++;
-        for (int i = 0; i < digit; i++)
+        for (int i = 0; i < digit; i++) //Computes the next Fibonacci number
         {
-            trash=num1[i]+num2[i];
+            temp=num1[i]+num2[i];
             num1[i]=num2[i];
-            num2[i]=trash;
+            num2[i]=temp;
         }
-        for (int i = 0; i < digit; i++)
+        for (int i = 0; i < digit; i++)// Propagates carry to the next digit and computes modulus
         {
             if (num1[i]>=10)
             {
@@ -31,11 +31,7 @@ int main()
             {
                 num2[i+1]+=num2[i]/10;
                 num2[i]=num2[i]%10; 
-                if (i==digit-1)
-                {
-                    cout << digit<<endl;
-                    digit++;
-                }
+                if (i==digit-1)    digit++;
             }
         }
     }
