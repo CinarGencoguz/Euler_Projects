d6639fc787c5e3be3b85693c04e61d14cd689c93
Çınar Gençoğuz <cinargencoguz01@outlook.com>
2026-09-08 09:20:20 +0300

    refactor:Euler686

--- src/Euler686.cpp
+++ src/Euler686.cpp
@@ -5,18 +5,18 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
-double a1;
+double temp1;
 long int power;
-int prob,a2;
+int counter,temp2;
 int main()
 {
-    prob=678910; power=1;
-    while(prob!=0)
+    counter=678910; power=1;
+    while(counter!=0)
     {
-        a1=power*log10(2);
-        a2=a1; a1-=a2;
-        if (a1>=log10(1.23)&&a1<log10(1.24))    prob--;
-        power++;
+        temp1=power*log10(2);  // program computes the logarithmic form of it. as an example 2^7 logaritmic form 2.107.... 
+        temp2=temp1; temp1-=temp2; //we are decreasing the number of digits as integer
+        if (temp1>=log10(1.23)&&temp1<log10(1.24))    counter--;// integer part shows us how many digits it has , decimal part if we take it as power of 10 as 10^x
+        power++;                                                // it'll show us starting number , I did it as a reverse. I took the logarithm of threshold and our number should be in it
     }
     cout << power-1;
 }
