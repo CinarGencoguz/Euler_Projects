a1aabf2979e6c5bdd7e84922cbe7b79540197742
Çınar Gençoğuz <cinargencoguz01@outlook.com>
2026-09-09 14:56:42 +0300

    refactor:Euler048

--- src/Euler048.cpp
+++ src/Euler048.cpp
@@ -3,61 +3,15 @@
 
 #include <iostream>
 using namespace std;
-int digit[3000]={0};
-int Tdigit[3000]={0};
-int g;
-
+long long int result=0,temp,mod=10000000000;//
 int main()
 {
-    Tdigit[0]=g=1;
-    for (int i = 1; i <= 1000; i++)
+    for (size_t i = 1; i <= 1000; i++) // start at 1 because I already declared first multiplication as temp=i
     {
-        for (int k = 0; k < i; k++)
-        {
-            for (int t = 0; t < g; t++)
-            {
-                Tdigit[t]*=i;
-            }
-            
-        for (int t = 0; t < g; t++)
-        {
-            if (Tdigit[t]>=10&&t==g-1)
-            {
-                Tdigit[t+1]+=Tdigit[t]/10;
-                Tdigit[t]=Tdigit[t]%10;
-                g++;
-                break;
-            }
-            if (Tdigit[t]>=10)
-            {
-                Tdigit[t+1]+=Tdigit[t]/10;
-                Tdigit[t]=Tdigit[t]%10;
-            }
-        }
-        }
-        for (int f = 0; f < g; f++)
-        {
-            digit[f]+=Tdigit[f];
-            Tdigit[f]=0;    
-        }
-        Tdigit[0]=1;
-        for (int t = 0; t < g; t++)
-        {
-            if (digit[t]>=10&&t==g-1)
-            {
-                digit[t+1]+=digit[t]/10;
-                digit[t]=digit[t]%10;
-                break;
-            }
-            if (digit[t]>=10)
-            {
-                digit[t+1]+=digit[t]/10;
-                digit[t]=digit[t]%10;
-            }
-        } 
+        temp=i;
+        if(i%10==0) continue;//eliminate multiple of 10 , as an exapmle 10^10 has 11 digits and last 10 digit are all zeros 
+        for (size_t k = 1; k < i; k++)  temp=((temp*i))%mod;//processing modulus prosedure prevents overflow
+        result=(result+(temp%mod))%mod;
     }
-    for (int f = 9; f >=0; f--)
-    {
-        cout << digit[f];
-    }
+    cout << result;
 }
