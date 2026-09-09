f3bbdd3b60ebb245c98eb89a9f7884878960252c
Çınar Gençoğuz <cinargencoguz01@outlook.com>
2026-09-09 09:03:33 +0300

    refactor:Euler033

--- src/Euler032.cpp
+++ src/Euler032.cpp
@@ -5,17 +5,18 @@
 #include <iostream>
 #include <math.h>
 #include <set>
+#include <numeric>
 using namespace std;
-int result,num,temp,d;
+int result,num,temp;
+set <int> numbers;
 
-bool digitCounter(int a, int b, int c,int& d)
+bool digitCounter(int a, int b, int c) //  checks each element's digits and stores them
 {
 	set<int> digits;
     int temp;
-	while (a>=1)		{temp = a%10; digits.insert(temp);	a/=10;if(temp==0) return false ;}
-	while (b>=1)		{temp = b%10;digits.insert(temp);	b/=10;if(temp==0) return false ;}
-	while (c>=1)		{temp = c%10;digits.insert(temp);	c/=10;if(temp==0) return false ;}
-    d=digits.size();
+	while (a>=1)	{temp = a%10; digits.insert(temp);	a/=10;if(temp==0) return false ;}
+	while (b>=1)	{temp = b%10;digits.insert(temp);	b/=10;if(temp==0) return false ;}
+	while (c>=1)	{temp = c%10;digits.insert(temp);	c/=10;if(temp==0) return false ;}
 	if (digits.size()!=9)	return false;
 	return true;
 }
@@ -26,12 +27,12 @@ int main()
 	{
 		for (size_t t = 100; t < 10000; t++)
 		{
-			num=i*t; temp=(int)log10(num)+1;
-			if (temp!=4) continue;
-			if(digitCounter(i,t,num,d)) cout << d << "    "<< num << endl;
-            
+			num=i*t; 
+			temp=(int)log10(num)+1; // computes digit by using log10 (formula -> log10(x)+1 integer part is number of digits)
+			if (temp>4) continue;
+			if(digitCounter(i,t,num)) {numbers.insert(num); cout << num << endl;} // stores in set to prevent duplicate entries
 		}
 	}
-	
-	cout << result;
+	result = accumulate(numbers.begin(),numbers.end(), 0); // sums all elements of set
+	cout << endl<<result;
 }
--- src/Euler033.cpp
+++ src/Euler033.cpp
@@ -5,53 +5,20 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
-int denominator,numerator;
-double a,b,c;
-
-bool IsPrime(int a)
-{
-    if(a < 2) return false;
-    if(a == 2) return true;
-    if(a % 2 == 0) return false;
-    for (size_t i = 3; i <= sqrt(a)+1; i++)
-    {
-        if (a%i==0)
-            return false;
-    }
-    return true;
-}
+double result=1,a,b,c; 
 
 int main()
-{//i/k==(i/10)/(k%10)      i/k==(k/10)/(i%10)
-    denominator=numerator=1;
+{//i/k==(i/10)/(k%10)      i/k==(k/10)/(i%10)   Checks if cancelling cross digits yields an equivalent fraction
     for (int i = 10; i < 100; i++)
     {
-        for (int k = 10; k < 100; k++)
+        for (int k = i+1; k < 100; k++)
         {
-            if (i%10==0||k%10==0)
-                continue;
-            if (i<k)
+            if (i%10==0||k%10==0) continue;
+            if (i%10==k/10||i/10==k%10) // Checks whether pay and denominator share a common digit
             {
-                if (i%10==k/10||i/10==k%10)
-                {
-                    if (((double)i/k==(double)(i/10)/(k%10))||((double)i/k==(double)(i%10)/(k/10)))
-                    {
-                        numerator*=i;denominator*=k;
-                        cout << i << "      "<<k<<endl;
-                    }
-                }  
-            }
+                if (((double)i/k==(double)(i/10)/(k%10))||((double)i/k==(double)(i%10)/(k/10)))     result=(result/i)*k; // Validates if digit cancelling yields the correct mathematical value
+            } 
         }
     }
-    for (int i = 2; i < 98; i++)
-    {
-        if (IsPrime(i)&&numerator%i==0&&denominator%i==0)
-        {
-            while (numerator%i==0&&denominator%i==0)
-            {
-                numerator/=i;denominator/=i;
-            }
-        }
-    }
-    cout << denominator;
+    cout << result;
 }
