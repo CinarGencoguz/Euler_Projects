cecbbc1f64dca1e4357d7a72fee0706941333414
Çınar Gençoğuz <cinargencoguz01@outlook.com>
2026-09-15 10:08:10 +0300

    refactor:IsPrime

--- include/MathUtils.hpp
+++ include/MathUtils.hpp
@@ -14,7 +14,7 @@ namespace Mathutilus
         if(a == 2 || a == 3 ) return true;      
         if(a % 2 == 0 || a % 3 == 0) return false;      //eliminate number can divided by 2 and 3
         if((a+1) % 6 != 0 && (a-1) % 6 != 0) return false;      //a prime number must be form in 6k+1 or 6k-1 beside 2 and 3
-        for (size_t i = 3; i <= sqrt(a)+1; i+=2)        //if a number doesn't have a divider less than sqrt of it plus one mathematically it can't have one
+        for (size_t i = 5; i <= sqrt(a)+1; i+=2)        //if a number doesn't have a divider less than sqrt of it plus one mathematically it can't have one
         {                                               // because of that our threshold is sqrt of number plus 1
             if (a%i==0) return false;
         }
